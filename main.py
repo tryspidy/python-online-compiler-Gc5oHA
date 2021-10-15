567964efd3067d3a76f953758c55405660f97b0d
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-15 12:12:57 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# some best python compilers are:
+https://repl.it/  # best online compiler
+https://www.onlinegdb.com/online_python_compiler/
+https://www.tutorialspoint.com/execute_python_online.php/
+https://www.programiz.com/python-programming/online-compiler/
